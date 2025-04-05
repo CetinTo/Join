d64893340ae04b9a5b467096f9c53df88bbca6d2
Asim-taskin <asim.t@gmx.net>
2025-04-05 18:04:41 +0200

    update - folder - structure

--- scripts/datepicker.js
+++ scripts/datepicker.js
@@ -25,9 +25,33 @@ function openDatePicker(inputId) {
       });
       dateInput._flatpickr.open();
     }
-  }
+}
   
+
+function openDatePickerAddTask(inputId) {
+  let dateInput = document.getElementById(inputId);
+  if (!dateInput) return;
   
+
+  dateInput.setAttribute("readonly", "readonly");
+
+  flatpickr(dateInput, {
+    dateFormat: "d/m/Y",
+    defaultDate: "today",
+    minDate: "today",
+    disableMobile: true, 
+    locale: flatpickr.l10ns.de,
+    allowInput: false,
+    onChange: function(selectedDates, dateStr, instance) {
+      if (selectedDates.length > 0 && selectedDates[0] < new Date().setHours(0, 0, 0, 0)) {
+        instance.setDate("today", true);
+        alert("Das ausgewählte Datum liegt in der Vergangenheit. Bitte wählen Sie ein aktuelles Datum.");
+      }
+    }
+  });
+  
+  dateInput.focus();
+}
 
 
 
@@ -42,3 +66,5 @@ function setPriority(priority) {
         selectedButton.classList.add('active');
     }
 }
+
+
